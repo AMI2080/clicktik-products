952cee499cc59eb89aea35bac6ac4eca6891b749
AMI2080 <AMI_2080@Yahoo.com>
2024-08-11 18:13:29 +0300

    create product component

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { FormsModule } from '@angular/forms';
 import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/products/product/product.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, ProductComponent],
   imports: [CommonModule, FormsModule, ProductsRoutingModule],
 })
 export class ProductsModule {}
